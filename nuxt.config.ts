cc0ebb82607d66d69dd86d220fb96b3fe3d8a4ec
VinumWeb <admin@vinumweb.com>
2024-05-24 10:35:08 +0200

    Added more defaults

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,11 +7,25 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/ui',
     '@formkit/auto-animate/nuxt',
-    '@nuxt/image'
+    '@nuxt/image',
+    '@nuxtjs/seo',
+    '@nuxtjs/plausible',
+    'nuxt-delay-hydration',
+    'nuxt-typed-router',
+    '@nuxtjs/eslint-module'
   ],
+  delayHydration: {
+    debug: process.env.NODE_ENV === 'development'
+  },
   runtimeConfig: {
     facebookAccessToken: '',
     facebookPageId: '',
+  },
+  plausible: {
+    apiHost: 'https://analytics.vinumweb.com'
+  },
+  seo: {
+    redirectToCanonicalSiteUrl: true
   },
   ui: {
     icons: ['tabler', 'simple-icons', 'logos']
